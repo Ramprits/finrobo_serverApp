bcacd8fad4a437a9b2de3d98d07a47e67f1c15c3
Ramprit Sahani <rampritsahani@outlook.in>
2018-10-14 10:42:09 +0530

    remove database

--- startup/db.js
+++ startup/db.js
@@ -1,11 +1,11 @@
-const winston = require('winston');
-const mongoose = require('mongoose');
-const config = require('config');
+const winston = require( 'winston' );
+const mongoose = require( 'mongoose' );
+const config = require( 'config' );
 
-module.exports = function() {
-  const db = config.get('db');
-  mongoose.connect(db)
-    .then(() => winston.info(`Connected to ${db}...`));
+module.exports = function () {
+  const db = config.get( 'db' );
+  mongoose.connect( db, {
+      useNewUrlParser: true
+    } )
+    .then( () => winston.info( `Connected to ${db}...` ) );
 }
-
-// mongodb://ramprit:ramprit1234@ds153677.mlab.com:53677/mydatabase123
